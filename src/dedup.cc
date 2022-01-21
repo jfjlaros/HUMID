378a30793cffa1f6a298974c0b25798c59beae6e
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2022-01-21 10:39:15 +0100

    API documentation.

--- src/dedup.cc
+++ src/dedup.cc
@@ -9,6 +9,9 @@ using std::ios;
 using std::ofstream;
 using std::string;
 
+/*!
+ * Leaf structure for neighbour finding.
+ */
 struct NLeaf : Leaf {
   vector<size_t> lines;
   vector<NLeaf*> neighbours;
@@ -16,6 +19,12 @@ struct NLeaf : Leaf {
 };
 
 
+/*!
+ * Traverse neighbours to assign cluster IDs.
+ *
+ * \param leaf Leaf node.
+ * \param cluster Cluster ID.
+ */
 void assignCluster(NLeaf* leaf, size_t cluster) {
   leaf->cluster = cluster;
   for (NLeaf* neighbour: leaf->neighbours) {
@@ -25,6 +34,14 @@ void assignCluster(NLeaf* leaf, size_t cluster) {
   }
 }
 
+/*!
+ * Write a task start message to a log.
+ *
+ * \param log Log file.
+ * \param message Message.
+ *
+ * \return Task start time.
+ */
 time_t startMessage(ofstream& log, char const message[]) {
   log << message << "... ";
   log.flush();
@@ -32,14 +49,30 @@ time_t startMessage(ofstream& log, char const message[]) {
   return time(NULL);
 }
 
+/*!
+ * Write a task end message to a log.
+ *
+ * \param log Log file.
+ * \param start Task start time.
+ */
 void endMessage(ofstream& log, time_t start) {
   unsigned int seconds = (unsigned int)difftime(time(NULL), start);
   log << "done. (" << seconds / 60 << 'm' << seconds % 60 << "s)\n";
   log.flush();
 }
 
+/*!
+ * Determine duplicates.
+ *
+ * \param inputName File containing binary encoded reads.
+ * \param length Read length.
+ * \param distance Maximum hamming distance between reads.
+ * \param outputName Output file.
+ * \param logName Log file.
+ */
 void dedup(
-    string inputName, size_t length, string outputName, string logName) {
+    string inputName, size_t length, size_t distance,
+    string outputName, string logName) {
   Trie<4, NLeaf> trie;
 
   ofstream log(logName.c_str(), ios::out | ios::binary);
@@ -52,13 +85,16 @@ void dedup(
   endMessage(log, start);
 
   start = startMessage(log, "Calculating neighbours");
+  size_t nonDuplicates = 0;
   for (Result<NLeaf> walkResult: trie.walk()) {
-    for (Result<NLeaf> hammingResult: trie.hamming(walkResult.path, 1)) {
+    for (Result<NLeaf> hammingResult: trie.hamming(
+        walkResult.path, distance)) {
       if (walkResult.leaf != hammingResult.leaf) {
         walkResult.leaf->neighbours.push_back(hammingResult.leaf);
         hammingResult.leaf->neighbours.push_back(walkResult.leaf);
       }
     }
+    nonDuplicates++;
   }
   endMessage(log, start);
 
@@ -81,10 +117,22 @@ void dedup(
   output.close();
   endMessage(log, start);
 
+  log
+    << "\nRead " << line << " lines of length " << length << ".\n"
+    << "Found " << cluster - 1 << " clusters.\n"
+    << "Unique after perfect deduplication: " << nonDuplicates << " ("
+      << 100 * (float)nonDuplicates / line << "%).\n"
+    << "Unique after nonperfect deduplication (distance " << distance
+      << "): " << cluster - 1 << " ("
+      << 100 * (float)(cluster - 1) / line << "%).\n";
+
   log.close();
 }
 
 
+/*
+ * Argument parsing.
+ */
 int main(int argc, char* argv[]) {
   CliIO io(argc, argv);
 
@@ -93,6 +141,7 @@ int main(int argc, char* argv[]) {
     dedup, argv[0], "Deduplicate a dataset.", 
       param("input", "input file name"),
       param("length", "word length"),
+      param("-d", 1, "distance"),
       param("-o", "/dev/stdout", "output file name"),
       param("-l", "/dev/stderr", "log file name"));
 
--- src/ngs.cc
+++ src/ngs.cc
@@ -6,12 +6,28 @@ using std::cout;
 using std::ios;
 
 
+/*!
+ * Read a word from a file.
+ *
+ * \param handle File handle.
+ * \param size Word length.
+ *
+ * \return Word.
+ */
 vector<uint8_t> readWord(ifstream& handle, size_t size) {
   uint8_t buffer[size];
   handle.read((char*)buffer, size);
   return vector<uint8_t>(buffer, buffer + size);
 }
 
+/*!
+ * Read all words in a file.
+ *
+ * \param name File name.
+ * \param size Word length.
+ *
+ * \return All words.
+ */
 generator<vector<uint8_t>> readFile(char const name[], size_t size) {
   ifstream handle(name, ios::in | ios::binary);
   while (!handle.eof()) {
@@ -21,6 +37,11 @@ generator<vector<uint8_t>> readFile(char const name[], size_t size) {
   handle.close();
 }
 
+/*!
+ * Print a word.
+ *
+ * \param word Word.
+ */
 void printWord(vector<uint8_t>& word) {
   for (uint8_t letter: word) {
     cout << ' ' << (int)letter;
