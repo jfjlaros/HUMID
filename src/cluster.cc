f74e7ccf9a1e60b4f3f8b7f90e9447c4425c5ac0
Jeroen F.J. Laros <J.F.J.Laros@lumc.nl>
2022-06-13 21:45:29 +0200

    Update cluster.cc
    Some style related suggestions.

--- src/cluster.cc
+++ src/cluster.cc
@@ -11,7 +11,7 @@ Cluster::Cluster(size_t id) {
 
 
 /*!
- * Assign leaf to cluster
+ * Assign leaf to cluster.
  *
  * \param leaf Leaf node.
  * \param cluster Cluster.
@@ -22,7 +22,7 @@ void _assignLeaf(NLeaf* leaf, Cluster* cluster) {
 }
 
 /*
- * Update the counts for cluster
+ * Update the counts for cluster.
  *
  * \param leaf Leaf node.
  * \param cluster Cluster.
@@ -51,62 +51,57 @@ void assignMaxCluster(NLeaf* leaf, Cluster* cluster) {
 }
 
 /*!
- * Determine if a is at least 2x b. This is used on the the count difference
+ * Determine if a is at least 2b. This is used on the the count difference
  * between a leaf and its neighbour. If this is the case, neighbour will be
  * treated as a PCR-amplified error of leaf.
  */
 bool _atLeastDouble(int a, int b) {
-    return a > 2 * b -1;
+  return a > 2 * b - 1;
 }
 
-/*!
+/*
  * The inverse of _atLeastDouble, used to determine if the neighbour is the
  * true sequence, and leaf a PCR error.
  */
 bool _atMostHalf(int a, int b) {
-    return _atLeastDouble(b, a);
+  return _atLeastDouble(b, a);
 }
 
 /*!
- * Traverse neigbhours until a local maximum is reached
+ * Traverse neigbhours until a local maximum is reached.
  *
  * \param leaf Leaf node.
  */
+NLeaf* max_neighbour(NLeaf* leaf){
+  int i = 0;
+  while (i < leaf->neighbours.size()) {
+    NLeaf* neighbour = leaf->neighbours[i++];
 
-NLeaf * max_neighbour(NLeaf* leaf){
-    int i = 0;
-    while (i < leaf->neighbours.size()) {
-        NLeaf* neighbour = leaf->neighbours[i];
-        // Go to the neighbour and repeat
-        if (_atLeastDouble(neighbour->count, leaf->count) and !neighbour->cluster) {
-            leaf = neighbour;
-            i = 0;
-        }
-        // Try the next neighbour
-        else {
-            i++;
-        }
+    if (not neighbour->cluster and _atLeastDouble(neighbour->count, leaf->count)) {
+      // Go to the neighbour and repeat.
+      leaf = neighbour;
+      i = 0;
     }
-    return leaf;
+  }
+  return leaf;
 }
 
-/*!
+/*
  * Internal function to traverse neighbours to assign cluster ID
  *
  * \param leaf Leaf node.
  * \param cluster Cluster.
  */
 void _assignDirectionalCluster(NLeaf* leaf, Cluster* cluster) {
-  // Assign the leaf to the cluster
   _assignLeaf(leaf, cluster);
   for (NLeaf* neighbour: leaf->neighbours) {
     // If we encounter a neighbour that is unassigned, at most half leaf's
-    // size, we recursively add it to cluster
+    // size, we recursively add it to cluster.
     if (!neighbour->cluster and _atMostHalf(neighbour->count, leaf->count)) {
       // If the neighbour has less than half of the number of reads, the
-      // neighbour belongs to the current cluster
+      // neighbour belongs to the current cluster.
       _assignDirectionalCluster(neighbour, cluster);
-      }
+    }
   }
 }
 
@@ -120,7 +115,6 @@ void _assignDirectionalCluster(NLeaf* leaf, Cluster* cluster) {
  * \param cluster Cluster.
  */
 void assignDirectionalCluster(NLeaf* leaf, Cluster* cluster){
-  // Determine a maximum neighbour
   NLeaf* node = max_neighbour(leaf);
   // Update the maxcount for the cluster using the max node (only once)
   _updateMaxCount(node, cluster);
