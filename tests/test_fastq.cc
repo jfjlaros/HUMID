2242aaf5eaa75a4dc4269263daba4f945f2e448d
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2022-08-08 16:49:05 +0200

    Add edge cases to tests

--- tests/test_fastq.cc
+++ tests/test_fastq.cc
@@ -13,6 +13,9 @@ TEST_CASE("Extract UMI from FastQ header") {
   REQUIRE(_extractUMI("@header") == "");
   REQUIRE(_extractUMI("@header with spaces") == "");
   REQUIRE(_extractUMI("@header_with_many_underscores and space") == "");
+  REQUIRE(_extractUMI("@header_ignore_lowercase_umi_aatt") == "");
+  REQUIRE(_extractUMI("@header space then_underscore") == "");
+  REQUIRE(_extractUMI("@header space then_underscore_AATT") == "");
 
   // Tests for reads with UMI in header
   REQUIRE(_extractUMI("@header_AATT") == "AATT");
