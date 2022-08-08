aeb027e2986203fe82e85bcabd4ae852ae2a168f
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2022-08-08 13:42:52 +0200

    Add fakeRead struct for testing

--- tests/test_fastq.cc
+++ tests/test_fastq.cc
@@ -1,8 +1,17 @@
 #include <catch.hpp>
 
 #include "../src/fastq.h"
+#include "test_fastq.h"
 
 
 TEST_CASE("Dummy test") {
-  REQUIRE(true);
+  fakeRead *read = new fakeRead(
+      new string("@header"),
+      new string("ATCG")
+  );
+
+  // Access 'read' properties through the pointer, to mimic usage of fastp
+  REQUIRE(*read->mName == "@header");
+  REQUIRE(*read->mSeq == "ATCG");
+  delete read;
 }
